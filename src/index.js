803486bec493cd2dbd3b12ec670ff8ddd639d5f7
Võ Văn Khoa <khoazero123@gmail.com>
2021-05-11 10:41:34 +0700

    Init

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
-// this import statement tells webpack to include styles.css in the build
-import css from "./styles.css"
 
 function init() {
   if (!window.addEventListener) return // Check for IE9+
 
+  const isPreview = INSTALL_ID === 'preview'
   let options = INSTALL_OPTIONS
-  let element
+
+  let loadLogRocket = false;
+
+  const vendorScript = document.createElement("script")
+  vendorScript.src = "https://cdn.lr-ingest.io/LogRocket.min.js"
+  document.head.appendChild(vendorScript)
+  vendorScript.onload = function () {
+    loadLogRocket = true;
+    updateElement();
+  }
 
   // updateElement runs every time the options are updated.
   // Most of your code will end up inside this function.
   function updateElement() {
-    element = INSTALL.createElement(options.location, element)
+    if (!loadLogRocket) {
+      isPreview && console.error("LogRocket not load yet")
+      return
+    }
+    if (!options.id) {
+      isPreview && console.error("LogRocket ID not set")
+      return
+    }
 
-    // Set the app attribute to your app's dash-delimited alias.
-    element.setAttribute("app", "your-app-name")
-    element.innerHTML = options.message
+    isPreview && console.log(`LogRocket init ${options.id}`)
+    window.LogRocket.init(options.id)
   }
 
   // INSTALL_SCOPE is an object that is used to handle option changes without refreshing the page.
